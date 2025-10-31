e53446b306a3f372ea2dc40380839084466cdfa0
[dyad] <rroa2513@gmail.com>
2025-10-30 20:25:51 -0500

    [dyad] He añadido polyfills para Node.js y configurado Vite para resolver el error "Buffer is not defined" al usar `csv-stringify` en el navegador. - wrote 4 file(s), added buffer, stream-browserify, util, @esbuild-plugins/node-globals-polyfill, @esbuild-plugins/node-modules-polyfill package(s)

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,6 +15,25 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      // Añadir polyfills para módulos core de Node.js
+      'buffer': 'buffer/',
+      'stream': 'stream-browserify',
+      'util': 'util/',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Habilitar plugins de polyfill de esbuild
+      define: {
+        global: 'globalThis', // Definir global para compatibilidad con el navegador
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
     },
   },
 }));
