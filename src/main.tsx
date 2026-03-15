4b6f6fbcc077c4b3c1a1e8f13dd4b7cec40a2711
Rafael Roa <rroa2513@gmail.com>
2026-03-14 23:52:05 -0500

    Triple-layer polyfill fix for Buffer error in production

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
+import { Buffer } from "buffer";
 import App from "./App.tsx";
 import "./index.css";
+
+// Force Buffer global for third-party libraries
+if (typeof window !== 'undefined') {
+  window.Buffer = Buffer;
+  window.global = window;
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
